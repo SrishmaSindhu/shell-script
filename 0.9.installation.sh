727b64fc2c1fb2d10123e3d32f036ac1d805255e
Sindhuja Addala <sindhu.a@saince.in>
2024-04-18 18:58:23 +0530

    functions

--- 0.9.installation.sh
+++ 0.9.installation.sh
@@ -10,24 +10,15 @@ else
     echo "You are super user."
 fi
 
-dnf install mysql -y
+VALIDATE(){
+echo "exit status:$1"
+echo "now doing:$2"
+}
 
-if [ $? -ne 0 ]
-then
-    echo "Installation of mysql...FAILURE"
-    exit 1
-else
-    echo "Installation of mysql...SUCCESS"
-fi
+dnf install mysql -y
+VALIDATE $? "installing My SQL"
+
 
 dnf install git -y
+VALIDATE $? "installing git"
 
-if [ $? -ne 0 ]
-then
-    echo "Installation of git...FAILURE"
-    exit 1
-else
-    echo "Installation of Git...SUCCESS"
-fi
-
-echo "is script proceeding?"
