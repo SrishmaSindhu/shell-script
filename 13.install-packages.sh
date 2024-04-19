d2d739540b47c59117d218366fc690a32e08e6c0
Sindhuja Addala <sindhu.a@saince.in>
2024-04-19 12:12:05 +0530

    functions

--- 13.install-packages.sh
+++ 13.install-packages.sh
@@ -4,10 +4,10 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%s)
 SCRIPTNAME=$( echo $0 | cut -d "." -f1 )
 LOGFILE=$TIMESTAMP-$SCRIPTNAME.log
-R=\e[31m
-G=\e[32m
-y=\e[33m
-N=\e[0m
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
 
 
 if [ $USERID -ne 0 ]
