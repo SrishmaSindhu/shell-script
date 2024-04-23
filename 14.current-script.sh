0e7c77a75fe3191b9eaeda9a43110ee2cd66b3bb
Sindhuja Addala <sindhu.a@saince.in>
2024-04-23 14:20:27 +0530

    mysql

--- 14.current-script.sh
+++ 14.current-script.sh
@@ -6,6 +6,8 @@ echo "before calling other script:$variable"
 echo "process instance id of current script:$$"
 
 
-./15.other-script.sh
+#./15.other-script.sh
+source ./15.other-script.sh
+
 
 echo "after calling other script:$variable"
