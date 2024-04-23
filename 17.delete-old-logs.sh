3ce8187e87cdecf67323cea36fdfaf1e74b8c6df
Sindhuja Addala <sindhu.a@saince.in>
2024-04-23 22:23:01 +0530

    mysql

--- 17.delete-old-logs.sh
+++ 17.delete-old-logs.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-SOURCE_DIRECTORY=
+SOURCE_DIRECTORY=/tmp/app-logs
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURCE_DIRECTORY ]
+then
+   echo "$G source directory exist"
+else
+   echo "$R Please make sure $SOURCE_DIRECTORY exists $N"
+   exit 1
+fi   
